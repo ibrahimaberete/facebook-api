b18f7a5263bd54832b0f3fcfd962dbc6c38e36de
ibrahimaberete <47101718+ibrahimaberete@users.noreply.github.com>
2022-02-22 21:35:09 +0100

    finished

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -10,7 +10,7 @@ const api = Router();
 
 api.use("/v1/authentication", authentication);
 api.use("/v1/users", jwtMiddleware({'secret':"SECRET"}),user );
-api.use("v1//posts",jwtMiddleware({'secret':"SECRET"}),post);
+api.use("/v1/posts",jwtMiddleware({'secret':"SECRET"}),post);
 
 
 export default api;
